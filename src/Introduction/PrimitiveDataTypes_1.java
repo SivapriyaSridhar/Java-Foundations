9f5c4327f0d6b006a663b2eaf22c85d83f8fb605
Sivapriya_Sridhar <sivapriya.sridhar@oracle.com>
2025-10-05 18:28:04 +0530

    Project 2: Primitive Data Types

---
+++ src/Introduction/PrimitiveDataTypes_1.java
@@ -0,0 +1,13 @@
+package Introduction;
+
+public class PrimitiveDataTypes_1 {
+    public static void main(String[] args) {
+        /* Primitive Data Types */
+        System.out.println("Byte value:\n Min:"+ Byte.MIN_VALUE+ " Max:"+ Byte.MAX_VALUE);
+        System.out.println("Short value:\n Min:"+ Short.MIN_VALUE+ " Max:"+ Short.MAX_VALUE);
+        System.out.println("Int value:\n Min:"+ Integer.MIN_VALUE+ " Max:"+ Integer.MAX_VALUE);
+        System.out.println("Long value:\n Min:"+ Long.MIN_VALUE+ " Max:"+ Long.MAX_VALUE);
+
+
+    }
+}
